b5f23276863120d9dc12af50b55bcfc6db2cffb8
Hailey Tran <101952089+hailey-tran-9@users.noreply.github.com>
2024-02-28 12:48:18 -0800

    [Part 5] Finished

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -214,34 +214,30 @@ namespace CGL
 	/* BEFORE FLIP */
      
     // Halfedges:
-     
     HalfedgeIter h0 = e0->halfedge();
 	HalfedgeIter h1 = h0->next();
 	HalfedgeIter h2 = h1->next();
 	HalfedgeIter h3 = h0->twin();
 	HalfedgeIter h4 = h3->next();
 	HalfedgeIter h5 = h4->next();
-	HalfedgeIter h6 = h1->twin();
-	HalfedgeIter h7 = h2->twin();
-	HalfedgeIter h8 = h4->twin();
-	HalfedgeIter h9 = h5->twin();
+	HalfedgeIter h6 = h2->twin();
+	HalfedgeIter h7 = h1->twin();
+	HalfedgeIter h8 = h5->twin();
+	HalfedgeIter h9 = h4->twin();
 
 	// Vertices:
-
-	VertexIter v0 = h0->vertex();
-	VertexIter v1 = h3->vertex();
+	VertexIter v0 = h3->vertex();
+	VertexIter v1 = h0->vertex();
 	VertexIter v2 = h2->vertex();
 	VertexIter v3 = h5->vertex();
 
 	// Edges:
-
-	EdgeIter e1 = h1->edge();
-	EdgeIter e2 = h2->edge();
-	EdgeIter e3 = h4->edge();
-	EdgeIter e4 = h5->edge();
+	EdgeIter e1 = h2->edge();
+	EdgeIter e2 = h1->edge();
+	EdgeIter e3 = h5->edge();
+	EdgeIter e4 = h4->edge();
 
 	// Faces:
-
 	FaceIter f0 = h0->face();
 	FaceIter f1 = h3->face();
 
@@ -262,79 +258,64 @@ namespace CGL
 
     VertexIter v4 = newVertex();
     v4->position = (v1->position + v0->position) / 2.0f;
-    v4->halfedge() = h0;
+    v4->halfedge() = h3;
 
     EdgeIter e5 = newEdge();
-    e5->halfedge() = h11;
+    e5->halfedge() = h12;
     EdgeIter e6 = newEdge();
-    e6->halfedge() = h2;
+    e6->halfedge() = h5;
     EdgeIter e7 = newEdge();
-    e7->halfedge() = h4;
+    e7->halfedge() = h1;
 
     FaceIter f2 = newFace();
-    f2->halfedge() = h3;
+    f2->halfedge() = h5;
     FaceIter f3 = newFace();
-    f3->halfedge() = h10;
+    f3->halfedge() = h12;
 
-    h10->setNeighbors(h11, h4, v3, e7, f3);
-    h11->setNeighbors(h12, h15, v4, e5, f3);
-	h12->setNeighbors(h10, h8, v0, e3, f3);
-    h13->setNeighbors(h14, h2, v4, e6, f1);
-	h14->setNeighbors(h15, h7, v2, e2, f1);
-	h15->setNeighbors(h13, h11, v0, e5, f1);
+    h10->setNeighbors(h11, h5, v4, e6, f3);
+    h11->setNeighbors(h12, h8, v3, e3, f3);
+	h12->setNeighbors(h10, h14, v0, e5, f3);
+    h13->setNeighbors(h14, h1, v2, e7, f1);
+	h14->setNeighbors(h15, h12, v4, e5, f1);
+	h15->setNeighbors(h13, h7, v0, e2, f1);
 
 
- //   /* Reassign pointers */
+    /* Reassign pointers */
  
- //   // Halfedges:
+    // Halfedges:
+    h0->setNeighbors(h1, h3, v1, e0, f0);
+    h1->setNeighbors(h2, h13, v4, e7, f0);
+	h2->setNeighbors(h0, h6, v2, e1, f0);
+	h3->setNeighbors(h4, h0, v4, e0, f2);
+	h4->setNeighbors(h5, h9, v1, e4, f2);
+	h5->setNeighbors(h3, h10, v3, e6, f2);
+	h6->setNeighbors(h6->next(), h2, h6->vertex(), h6->edge(), h6->face());
+	h7->setNeighbors(h7->next(), h15, h7->vertex(), h7->edge(), h7->face());
+	h8->setNeighbors(h8->next(), h11, h8->vertex(), h8->edge(), h8->face());
+	h9->setNeighbors(h9->next(), h4, h9->vertex(), h9->edge(), h9->face());
 
-    h0->setNeighbors(h0, h3, v4, e0, f0);
-    h1->setNeighbors(h2, h6, v1, e1, f0);
-	h2->setNeighbors(h0, h13, v2, e6, f0);
-	h3->setNeighbors(h4, h0, v1, e0, f2);
-	h4->setNeighbors(h5, h10, v4, e7, f2);
-	h5->setNeighbors(h3, h9, v3, e4, f2);
-	h6->setNeighbors(h6->next(), h1, h6->vertex(), h6->edge(), h6->face());
-	h7->setNeighbors(h7->next(), h14, h7->vertex(), h7->edge(), h7->face());
-	h8->setNeighbors(h8->next(), h12, h8->vertex(), h8->edge(), h8->face());
-	h9->setNeighbors(h9->next(), h5, h9->vertex(), h9->edge(), h9->face());
+    // Vertices:
+    v0->halfedge() = h12;
+    v1->halfedge() = h0;
+    v2->halfedge() = h13;
+    v3->halfedge() = h5;
+    v4->halfedge() = h3;
 
- //   // Vertices:
-
-    v0->halfedge() = h15;
-    v1->halfedge() = h3;
-    v2->halfedge() = h2;
-    v3->halfedge() = h10;
-    v4->halfedge() = h0;
-
- //   // Edges:
-
+    // Edges:
     e0->halfedge() = h0;
-	e1->halfedge() = h1;
-    e2->halfedge() = h14;
-    e3->halfedge() = h12;
-    e4->halfedge() = h5;
-    e5->halfedge() = h15;
-    e6->halfedge() = h2;
-    e7->halfedge() = h4;
+	e1->halfedge() = h2;
+    e2->halfedge() = h15;
+    e3->halfedge() = h11;
+    e4->halfedge() = h4;
+    e5->halfedge() = h14;
+    e6->halfedge() = h10;
+    e7->halfedge() = h13;
 
- //   // Faces:
-
+    // Faces:
     f0->halfedge() = h0;
     f1->halfedge() = h13;
-	f2->halfedge() = h3;
-    f3->halfedge() = h10;
-
-    check_for(v0);
-    check_for(v1);
-	check_for(v2);
-    check_for(v3);
-    check_for(v4);
-
-	check_for(f0);
-	check_for(f1);
-	check_for(f2);
-	check_for(f3);
+	f2->halfedge() = h5;
+    f3->halfedge() = h12;
 
     return v4;
   }
